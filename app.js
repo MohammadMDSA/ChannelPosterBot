a35ee9af7fb5bb7acc26b858e7cd77d0a8b9080d
Mohammad MDSA <samimd.77@gmail.com>
2017-11-16 01:10:53 +0330

    Cleaning up debuging messages

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('dotenv-extended').load();
-
+const { setInterval } = require('timers');
 const builder = require('botbuilder');
 const restify = require('restify');
 const { Prompts, UniversalBot, ChatConnector, Message } = builder;
@@ -18,6 +18,56 @@ let connector = new ChatConnector({
 });
 server.post('/api/messages', connector.listen());
 
+let timer;
+let counter = 0;
+
 let bot = new UniversalBot(connector, (session) => {
-	// bot code goes here
+	session.send(session.conversationData.isActive + " begg");
+	session.conversationData.count = 0;
+	if (!session.conversationData.isActive)
+		session.replaceDialog('root');
 });
+
+bot.dialog('root', [
+	(session) => {
+		session.send('inside root dialog');
+		session.send('before if ' + counter);
+		if (session.message.address.conversation.id == process.env.TARGET_CHANNEL_ID) {
+			session.send('inside if');
+			session.beginDialog('proActive');
+		}
+		else {
+			session.send('inside else');
+			// session.userData.isActive = false;
+			session.endConversation();
+		}
+	}
+]);
+
+bot.dialog('proActive', (session) => {
+	// session.userData.isActive = true;
+	if (counter > 0)
+		session.endConversation();
+	session.send('starting...');
+	counter++;
+	session.send(counter + " count");
+	timer = setInterval(() => {
+		db.find('pendingMessages', {}, (results) => {
+			results.forEach((item) => {
+				let msg = item.message;
+				session.send(msg);
+			});
+			db.deleteManyDocuments('pendingMessages', {});
+		});
+	}, 10000);
+});
+
+bot.dialog('ender', (session) => {
+	// session.userData.isActive = false;
+	session.endConversation('The End');
+	counter = 0;
+	clearInterval(timer);
+})
+	.triggerAction({
+		matches: /^end$/i
+	});
