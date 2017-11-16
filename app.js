d7916b9b2a6503e3b66edb67fa87cef75b3b4e60
Mohammad MDSA <samimd.77@gmail.com>
2017-11-16 09:14:55 +0330

    Handled multi-session for single thread

--- app.js
+++ app.js
@@ -22,10 +22,6 @@ let timer;
 let counter = 0;
 
 let bot = new UniversalBot(connector, (session) => {
-	session.send(session.conversationData.isActive + " begg");
-	session.conversationData.count = 0;
-	if (!session.conversationData.isActive)
-		session.replaceDialog('root');
 });
 
 bot.dialog('root', [
@@ -42,24 +38,31 @@ bot.dialog('root', [
 			session.endConversation();
 		}
 	}
-]);
+])
+	.triggerAction({
+		matches: /^start$/i
+	});
 
 bot.dialog('proActive', (session) => {
 	// session.userData.isActive = true;
-	if (counter > 0)
-		session.endConversation();
-	session.send('starting...');
-	counter++;
-	session.send(counter + " count");
-	timer = setInterval(() => {
-		db.find('pendingMessages', {}, (results) => {
-			results.forEach((item) => {
-				let msg = item.message;
-				session.send(msg);
+	if (counter !== 0) {
+		session.send('more than 1');
+		session.endDialog();
+	}
+	else {
+		session.send('starting...');
+		counter++;
+		session.send(counter + " count");
+		timer = setInterval(() => {
+			db.find('pendingMessages', {}, (results) => {
+				results.forEach((item) => {
+					let msg = item.message;
+					session.send(msg);
+				});
+				db.deleteManyDocuments('pendingMessages', {});
 			});
-			db.deleteManyDocuments('pendingMessages', {});
-		});
-	}, 10000);
+		}, 10000);
+	}
 });
 
 bot.dialog('ender', (session) => {
